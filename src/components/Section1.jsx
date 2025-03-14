96320452695590130f554949ba74ef17608f0c94
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2025-03-14 17:36:12 +0100

    FIXED: Section1 -> Tailwind

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,49 +1,19 @@
-import { Text, Button, VStack } from "@chakra-ui/react";
 import { handleScroll } from "../utils/handleScroll";
 
 function Section1() {
   return (
-    <VStack
-      as="section"
-      width={"100svw"}
-      height={"100svh"}
-      flexDirection={"column"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      gap={8}
-      bgImage="url(/images/citric_bg.jpg)"
-      backgroundRepeat={"repeat-x"}
-      backgroundPosition={"bottom"}
-    >
-      <Text fontSize={"120px"} color={"primary"} fontWeight={"600"}>
-        EAT HEALTHY
-      </Text>
-      <Text
-        fontSize={"36px"}
-        textAlign={"center"}
-        color={"primary"}
-        maxW={"1000px"}
-      >
+    <section className="flex flex-col justify-center items-center gap-8 w-[100svw] h-[100svh] bg-[url('/images/citric_bg.jpg')] bg-repeat-x bg-bottom">
+      <h1 className="text-[120px] text-primary font-semibold">EAT HEALTHY</h1>
+      <p className="text-[36px] text-center text-primary max-w-[1000px]">
         Descubre el sabor y la calidad de las mejores legumbres
-      </Text>
-      <Button
-        cursor={"pointer"}
-        mt={36}
-        border="none"
-        borderRadius={8}
-        bg="bgButton"
-        px={16}
-        py={8}
-        color={"primary"}
-        transition={"all 0.2s"}
-        _hover={{
-          bg: "bgButton_hover",
-        }}
+      </p>
+      <button
+        className="cursor-pointer mt-[144px] border-none rounded-[8px] bg-bgButton px-[32px] py-[16px] text-primary transition-all duration-200 hover:bg-bgButton_hover"
         onClick={() => handleScroll("products")}
       >
-        <p>Nuestros Productos</p>
-      </Button>
-    </VStack>
+        Nuestros Productos
+      </button>
+    </section>
   );
 }
 
