7097c00a802d285d338df2c08568fd2200ff48a0
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2025-05-08 15:09:27 +0200

    UPDATE: ParallaxImage is smaller on mobile

--- src/components/ParallaxImage.jsx
+++ src/components/ParallaxImage.jsx
@@ -1,6 +1,6 @@
 function ParallaxImage() {
   return (
-    <div className="relative h-[240px] overflow-hidden">
+    <div className="relative h-[120px] md:h-[200px] lg:h-[240px] overflow-hidden">
       <div className="fixed top-0 left-0 w-screen h-screen z-[-1]">
         <img
           src="/images/ParallaxImage.jpg"
